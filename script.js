673a7c82f60d320914a78c9ec251e1bbe7f7c40e
SofyenBhr <sofyenbhr@gmail.com>
2022-10-05 13:19:20 +0100

    Add chuck norris jokes array in script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const chuckNorrisJokes = [
+    "Chuck Norris doesn't read books. He stares them down until he gets the information he wants.",
+    "Time waits for no man. Unless that man is Chuck Norris.",
+    "If you spell Chuck Norris in Scrabble, you win. Forever.",
+    "Chuck Norris breathes air ... five times a day.",
+    "In the Beginning there was nothing ... then Chuck Norris roundhouse kicked nothing and told it to get a job."
+];
